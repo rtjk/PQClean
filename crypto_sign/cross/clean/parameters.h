56921a52255773ac3c5040974f6aed2ae7061f85
rtjk <47841774+rtjk@users.noreply.github.com>
2024-03-23 17:30:07 +0100

    moved "parameter set" defines to set.h, added fips to microsoft makefile

--- crypto_sign/cross/clean/parameters.h
+++ crypto_sign/cross/clean/parameters.h
@@ -25,15 +25,7 @@
 
 #pragma once
 #include <stdint.h>
-
-#define RSDP 1
-#define CATEGORY_1 1
-#define BALANCED 1
-
-/* #define AES_CTR_CSPRNG
-#define SHA3_HASH */
-/* #define OMIT_SEED_TREE ""
-#define KECCAK_EXTERNAL_ENABLE */
+#include "set.h"
 
 /******************************************************************************/
 /*************************** Base Fields Parameters ***************************/
---
+++ crypto_sign/cross/clean/set.h
@@ -0,0 +1,9 @@
+
+#define RSDP 1
+#define CATEGORY_1 1
+#define BALANCED 1
+
+/* #define AES_CTR_CSPRNG
+#define SHA3_HASH */
+/* #define OMIT_SEED_TREE ""
+#define KECCAK_EXTERNAL_ENABLE */
